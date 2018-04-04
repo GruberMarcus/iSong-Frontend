3b0db82fd4a0c6a168104925887fbf1533c613d8
David Lucuta <david.lucuta@edu.htl.rennweg.at>
2018-04-04 16:16:03 +0200

    Login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { User } from '../models/user';
+import { UserService } from '../services/user.service';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  users:User[] = [];
+  public user = new User();
+  public errorMsg = '';
+
+      constructor(
+          private _service:UserService, private _router:Router) {
+            //this.logoPath = 'assets/images/Logo.png';
+          }
+
+      logon() {
+        let response = this._service.logon(this.user)
+        .then ( resp => {
+          localStorage.setItem("name", this.user.name);
+          localStorage.setItem("token", JSON.parse(resp._body)["token"]);
+          this._router.navigate(['dashboard']);
+        });
+      }
+
+
+
+  ngOnInit() {
+    //console.log(localStorage);
+    document.body.classList.add('bg-img');
+
+    // Leitet zum Dashboard weiter, wenn der localStorage nicht leer ist.
+    if (localStorage.getItem("name") != null){
+        this._router.navigate(['dashboard']);
+    }
+  }
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from "@angular/http";
+import 'rxjs/Rx';
+import { Observable } from "rxjs";
+import { User } from '../models/user';
+import 'rxjs/add/operator/toPromise';
+import { Router } from '@angular/router';
+
+
+@Injectable()
+export class UserService {
+  API_URL = "http://localhost:8000/api/";
+
+  constructor(
+    private _router:Router, private http:Http) {
+    }
+
+  // Leert den LocalStorage und leitet zurück auf die Login-Seite.
+  logout() {
+    localStorage.clear();
+    this._router.navigate(['login']);
+  }
+
+  // Sollte der Eintrag "name" leer sein (also wenn das LocalStorage leer ist),
+  // wird er auf die Login-Seite zurückgeleitet. getToken() wird in jeder Service-
+  // Methode verwendet, um den korrekten HTTP-Pfad zu erhalten.
+  getToken() {
+    if (localStorage.getItem("name") === null){
+      this._router.navigate(['login']);
+    }
+    return localStorage.getItem('token');
+  }
+
+ 
+
+  // Führt eine POST-Methode aus in localhost:8000/api, um
+  // den Benutzer einzuloggen.
+  logon(user:any): Promise<any> {
+    return this.http
+     .post(this.API_URL, {name: user.name, password: user.password})
+     .toPromise()
+     .catch(this.handleError);
+  }
+
+
+  private handleError(error:any): Promise<any> {
+    console.error('An error occurred: ', error);
+    return Promise.reject(error.message || error);
+  }
+}
